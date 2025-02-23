98c05c9ac74af38a2e8ef31784e1600bc46e37bf
Aryaman Sharma <aryamansharma.india@gmail.com>
2025-02-23 07:10:10 +0530

    database

--- points-checklist-backend/config/db.js
+++ points-checklist-backend/config/db.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root', // Replace with your MySQL username
-  password: 'password', // Replace with your MySQL password
-  database: 'points_checklist',
+  host: 'bjsfo7jt6pm11bh9lqcl-mysql.services.clever-cloud.com',
+  user: 'uqm3tctr37xstiew', // Replace with your MySQL username
+  password: 'u3Z750WXy5mKah87sniR', // Replace with your MySQL password
+  database: 'bjsfo7jt6pm11bh9lqcl',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
